20dc2b7d00f8cc1f818e7ddd448988a283fec33d
Harish9999kumar <97820952+Harish9999kumar@users.noreply.github.com>
2022-09-23 13:22:42 +0530

    second commit

---
+++ src/test/java/Practice/Practice4Test.java
@@ -0,0 +1,11 @@
+package Practice;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Practice4Test {
+	@Test
+	public void practise4() {
+	Reporter.log("pracitse 4",true);
+	}
+}
