30e4dd1c49cfab405971aa3b10ad6b067f0c9493
Harish9999kumar <97820952+Harish9999kumar@users.noreply.github.com>
2022-09-23 11:49:17 +0530

    First commit

--- src/test/java/Practice/Practice3Test.java
+++ src/test/java/Practice/Practice3Test.java
@@ -1,5 +1,11 @@
 package Practice;
 
-public class Practice3Test {
+import org.testng.Reporter;
+import org.testng.annotations.Test;
 
+public class Practice3Test {
+	@Test
+	public void practise3() {
+	Reporter.log("pracitse 3",true);
+	}
 }
